f17c183c33f5b99ad4545ff6710c42041d9fc71d
Mark John Alcantara <MarkAlcantara@Aprils-MBP.ht.home>
2024-02-14 20:29:52 -0500

    initial folder layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Header from "./Layouts/Components/Header";
 
-export default function Home() {
+
+
+export default function Main() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+
+      <div>I am here</div>
+    
   );
 }
+
+
